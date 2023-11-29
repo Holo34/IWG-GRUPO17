0e1296b6eae73facc0aa6fd3e952bfd7073d0b22
José Miguel Contreras Ulloa <140554812+Y0tzi@users.noreply.github.com>
2023-11-29 15:13:12 -0300

    Delete SosPlaneta directory

--- SosPlaneta/Apps/Aplicacion1/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.home, name="home"), #Cada vez que queramos crear un nuevo URL dentro de esta página, agregamos un nuevo URL (Por ejemplo un "About Us")
-    path('mapa/', views.mapa, name="mapa"),
-    path('historial/', views.historial, name="historial"),
-    path('nosotros/', views.nosotros, name="nosotros"),
-    path('test/', views.test, name='test')
-]
